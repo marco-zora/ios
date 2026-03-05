a8d871343c2163a2ac25a6daf0b4200102767d52
marco-zora <marco-zora@users.noreply.github.com>
2026-03-05 13:49:00 +0000

    Deploy: 949da762ec414b97e0546e33761ea6c83ef7e835 949da762ec414b97e0546e33761ea6c83ef7e835

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+﻿// In development, always fetch from the network and do not enable offline support.
+// This is because caching would make development more difficult (changes would not
+// be reflected on the first load after each change).
+
+self.addEventListener('fetch', () => { });
+
+self.addEventListener('install', () => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
